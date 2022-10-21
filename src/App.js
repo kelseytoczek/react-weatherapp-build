9748269dd4c7ca8f89b8905f19105a36a2d4aa19
kelseytoczek <kelseytoczek@gmail.com>
2022-10-21 13:30:20 -0400

    footer added - links to github & portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a href="https://tubular-sprite-204a8e.netlify.app/" target="_blank">
+          Kelsey Toczek
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kelseytoczek/react-weatherapp-build"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
