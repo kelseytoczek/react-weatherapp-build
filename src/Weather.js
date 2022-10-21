b0f928a4139d629d95912eb1f6b7e02cfb75d3ba
kelseytoczek <kelseytoczek@gmail.com>
2022-10-21 15:15:15 -0400

    updated units of temp from metric to imperial

--- src/Weather.js
+++ src/Weather.js
@@ -33,7 +33,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
   }
 
